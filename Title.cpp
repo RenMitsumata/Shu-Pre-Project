3e61c79a12fc1ef7e8c8e865111b783ed3d8ca35
RenMitsumata <vqyw8ade4rrkjh8y4zjb@docomo.ne.jp>
2020-03-01 22:10:16 +0900

    インプットいじる前にセーブポイント タイトルもデファード化（画面出るようにした）

--- Title.cpp
+++ Title.cpp
@@ -12,6 +12,10 @@ Title::Title()
 
 Title::~Title()
 {
+	if (albedoPolygon) {
+		albedoPolygon->Uninit();
+		delete albedoPolygon;
+	}
 }
 
 void Title::Init()
@@ -19,6 +23,9 @@ void Title::Init()
 	UI* ui = AddGameObject<UI>(e_LAYER_UI);
 	ui->SetTextureAndSize("Assets/Textures/t_title002.png");
 	input = Manager::Get()->GetInput();
+	dxManager = Manager::Get()->GetDXManager();
+	albedoPolygon = new UI;
+	albedoPolygon->SetTextureAndSize(nullptr, XMFLOAT3(0.0f, 0.0f, 0.5f));
 }
 
 void Title::Update()
@@ -26,4 +33,16 @@ void Title::Update()
 	if (input->GetKeyTrigger(VK_SPACE)) {
 		Manager::Get()->ChangeScene(new Game);
 	}
+}
+
+void Title::DrawDeferred()
+{
+	ID3D11ShaderResourceView* srv[1];
+	srv[0] = dxManager->GetSRV(0);
+	if (srv[0] == nullptr) {
+		return;
+	}
+	albedoPolygon->SetDeferredTexture(srv[0]);
+	albedoPolygon->Draw();
+
 }
--- Title.h
+++ Title.h
@@ -2,15 +2,20 @@
 #include "Scene.h"
 
 class InputManager;
+class UI;
 
 class Title : public Scene
 {
 private:
 	InputManager* input;
+	DXManager* dxManager = nullptr;
+	UI* albedoPolygon = nullptr;
+
 public:
 	Title();
 	~Title();
 	void Init()override;
 	void Update()override;
+	void DrawDeferred()override;
 };
 
